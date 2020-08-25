78a973eb091dca1954eaaf25da20ccbf15c28a80
even <even311379@hotmail.com>
2020-08-26 01:03:16 +0800

    fix dev.py not ignore...

--- ebirdtaiwan/ebirdtaiwan/settings/dev.py
+++ ebirdtaiwan/ebirdtaiwan/settings/dev.py
@@ -1,13 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'czg!6epa5e&%+611hcflpd!%q^fl04t&azs^y5nub2sn)1^z4y'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['ebirdtaiwan.com'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
